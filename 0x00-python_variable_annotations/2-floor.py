4c952c13cada4f51152311e145d7b9b729fc75c4
oladakinjoye <oladakinjoye@gmail.com>
2024-07-04 15:08:13 -0700

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Basic annotations - floor.
+'''
+
+
+import math
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return math.floor(a)
