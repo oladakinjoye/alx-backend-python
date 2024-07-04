2c88099c1b61d8ceb6040a5404c320e5e597d273
oladakinjoye <oladakinjoye@gmail.com>
2024-07-04 15:11:51 -0700

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module for Basic annotations - to string
+'''
+
+
+def to_str(n: float) -> str:
+    '''Cast a floting type number in to a string.
+    '''
+    return str(n)
