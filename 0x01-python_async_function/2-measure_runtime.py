f2e851635a27e7dc2ff01d92fd4221aa86619312
oladakinjoye <oladakinjoye@gmail.com>
2024-07-08 14:17:44 -0700

    update measure runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-'''Measure the runtime
+'''Measure the runtime.
 '''
 import asyncio
+import time
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
-    '''Create an asynchronous task for wait_random.
+def measure_time(n: int, max_delay: int) -> float:
+    '''Compute the average runtime of wait_n.
     '''
-    return asyncio.create_task(wait_random(max_delay))
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
